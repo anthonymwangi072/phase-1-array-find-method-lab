7f830c30ee49f58ee52cf5808c030667dcff3923
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-08-21 11:12:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(record){
+   for(const array of record){
+    if(array.result === "W"){
+        return array.year
+    }
+   }
+}
+const sadReality = [{result: "N/A"}];
